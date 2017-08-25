8d4fc9efd7175cbcdedc76a10b42b9ec9bb8bd44
simon.castaneda <simon.castaneda@linctik.co>
2017-08-25 17:32:54 -0500

    entorno del servidor

--- server/index.js
+++ server/index.js
@@ -3,9 +3,17 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.use(express.static('client'));
+
 app.get('/hola-mundo', function(req, res){
 res.status(200).send('hola mundo desde una ruta ');
 });
+
+io.on('connection', function(socket){
+console.log("El cliente con IP: "+socket.handshake.address+"se ha conectado...");
+
+});
+
 
 server.listen(6677, function(){
     console.log('Servidor está funcionando en http://localhost:6677');
