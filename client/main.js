9282ba76ade266e8d3a3cac1aaedd4c02bcdeee8
simon.castaneda <simon.castaneda@linctik.co>
2017-08-30 16:46:28 -0500

    ajustes

--- client/main.js
+++ client/main.js
@@ -1,2 +1,34 @@
 
-var socket = io.connect('http://172.21.20.126/6677',{'forceNew':true});
+var socket = io.connect('http://172.21.20.142:6677', { 'forceNew': true });
+
+socket.on('messages', function (data) {
+
+    console.log(data);
+    render(data);
+});
+
+function render(data) {
+    var html = data.map(function (messages, index) {
+        return (`
+    <div class="message">
+            <strong>${messages.nickname}</strong> dice:
+            <p>${messages.text}</p>
+        </div>
+    `);
+    }).join('   ');
+    var div_msgs = document.getElementById('messages');
+    div_msgs.innerHTML = html;
+    div_msgs.scrollTop = div_msgs.scrollHeight;
+}
+
+
+function addMessage(e) {
+    var message = {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value
+    };
+
+    document.getElementById('nickname').style.display = 'none';
+    socket.emit('add-message', message)
+    return false;
+}
--- server/index.js
+++ server/index.js
@@ -5,16 +5,29 @@ var io = require('socket.io')(server);
 
 app.use(express.static('client'));
 
-app.get('/hola-mundo', function(req, res){
-res.status(200).send('hola mundo desde una ruta ');
+app.get('/hola-mundo', function (req, res) {
+    res.status(200).send('hola mundo desde una ruta ');
 });
 
-io.on('connection', function(socket){
-console.log("El cliente con IP: "+socket.handshake.address+"se ha conectado...");
+var messages = [{
+    id: 1,
+    text: 'benvenido al chat privado de socket.io y node js de simon castañeda',
+    nickname: 'siancafi1'
+}]
 
+io.on('connection', function (socket) {
+    console.log("El cliente con IP: " + socket.handshake.address + " se ha conectado...");
+    socket.emit('messages', messages);
+
+    socket.on('add-message', function(data){
+        messages.push(data);
+        io.sockets.emit('messages',messages);
+    });
 });
 
 
-server.listen(6677, function(){
+
+server.listen(6677, function () {
     console.log('Servidor está funcionando en http://localhost:6677');
+
 });
